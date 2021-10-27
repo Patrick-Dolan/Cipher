f71e3f053b57f4031a8db425ddc072f923bd6599
Patrick Dolan <dolanp1992@gmail.com>
2021-10-26 20:07:50 -0700

    add functions for more ciphers
    Co-authored-by: Ryan Rendon <Rendon.S.Ryan@gmail.com>
    Co-authored-by: Anastasia Han <anastasia.han@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,25 @@ function capitalFirstAndLast(response) {
   const firstAndLast = firstLetter + lastLetter;
   return result = firstAndLast.toUpperCase();
 }
+function capitalFirstAndLastSwap(response) {
+  const firstLetter = response.charAt(0);
+  const lastLetter = response.charAt(response.length - 1);
+  const firstAndLast = lastLetter + firstLetter;
+  return result = firstAndLast.toUpperCase();
+}
+function concatIt(response){
+  return response + capitalFirstAndLast(response);
+}
+function spaghetti(response) {
+  const number = Math.floor((response.length - 1) / 2);
+  const letter = response.charAt(number);
+  //return console.log(number);
+  return letter + concatIt(response);
+}
+function reverse(response) {
+  return response.split("").reverse().join("");
+}
 const response = prompt("Write a sentence:");
-alert(response);
 
-alert(capitalFirstAndLast(response));
+alert(reverse(spaghetti(response)));
 
