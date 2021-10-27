35927cb8c8fd7420f9ce9fad462115dec9282f49
Patrick Dolan <dolanp1992@gmail.com>
2021-10-26 19:43:37 -0700

    add a function that takes first and last letter of a string and capatilizes them then prints
    Co-authored-by: Ryan Rendon <Rendon.S.Ryan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
+function capitalFirstAndLast(response) {
+  const firstLetter = response.charAt(0);
+  const lastLetter = response.charAt(response.length - 1);
+  const firstAndLast = firstLetter + lastLetter;
+  return result = firstAndLast.toUpperCase();
+}
 const response = prompt("Write a sentence:");
 alert(response);
+
+alert(capitalFirstAndLast(response));
+
